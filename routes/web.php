3623c5a5302d899c27489c0abecda55a4024aae4
Atef Ramadan <ateframadan8822@gmail.com>
2024-05-15 16:13:23 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Alazzidev\LaraodooXmlrpc\Facades\Odoo;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $searchResult = Odoo::search('res.partner', []);
+    $records = Odoo::read('res.partner', $searchResult, ['name', 'id']);
+    dd($records);
     return view('welcome');
 });
